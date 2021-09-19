bf1445716f1cfd6fcd41640f835bbb45f7c4bc61
yahiko1y <mwarsame922@gmail.com>
2021-09-19 22:56:45 +0300

    Update ingame.py

--- ingame.py
+++ ingame.py
@@ -1,40 +1,82 @@
 import numpy as np
+
 def create_board():
     board=np.zeros((6,7))
     return board
 
-def win(board,p):
-    #horizantal
+def horizontal(board,p):
     for i in range(6):
         check=0
-        #print("horizontal board[{}]".format(i))
         for j in range(7):
             if(board[i][j]==p):
                 check+=1
-                #print("in horizontal check board[{}][{}] and check is {}".format(i,j,check))
-                if check==4:
-                    return True
-            else:
-                check=0
-                
-    #vertical
-    for i in range(7):
-        check=0
-        #print("vertical board[{}]".format(i))
-        for j in range(6):
-            if(board[j][i]==p):
-                check+=1
-                #print("in vertical check board[{}][{}] and check is {}".format(j,i,check))
-                
                 if check==4:
                     return True
             else:
                 check=0
     return False
-            
-            
-        
+
+def vertical(board,p):
+    for i in range(7):
+        check=0
+        for j in range(6):
+            if(board[j][i]==p):
+                check+=1
+                if check==4:
+                    return True
+            else:
+                check=0
+    return False
+
+def daigonal_right(board,p):
+    for j in range(6):
+        check=0
+        same=j
+        for x in range(5,-1,-1):
+            j=same
+            for i in range(x,-1,-1):
+                if j>6:
+                    continue
+                if(board[i][j]==p):
+                    check+=1
+                    if check==4:
+                            return True
+                    else:
+                        check=0
+                    j+=1
+    return False
+
+def daigonal_left(board,p):
+    for j in range(6,-1,-1):
+        check=0
+        same=j
+        for x in range(5,-1,-1):
+            j=same
+            for i in range(x,-1,-1):
+                if j<0:
+                    continue
+                if(board[i][j]==p):
+                        check+=1
+                        if check==4:
+                            return True
+                else:
+                    check=0
+                j-=1
+    return False
+
+def win(board,p):
     
+    if horizontal(board,p):
+        return True
+    if vertical(board,p):
+        return True
+    if daigonal_right(board,p):
+        return True
+    if daigonal_left(board,p):
+        return True
+
+    return False
+            
 
 continue_game = True
 
@@ -54,6 +96,7 @@ while continue_game:
             line = 5
             
         elif (board[line][p1]!= 0):
+            print("board[{}][{}] is occupied".format(line,p1))
             line -= 1
         else:
             board[line][p1] = 1
@@ -70,18 +113,23 @@ while continue_game:
         
 
     print(board)
+    
     p2 = int(input("player2 pick a line: "))
     player_2_turn = True
+    line = 5
     while player_2_turn:
+        
         if(line < 0):
             p2 = int(input("player2 that line is full: "))
             line = 5
             
         elif (board[line][p2]!= 0):
+            print("board[{}][{}] is occupied".format(line,p2))
             line -= 1
         else:
             board[line][p2] = 2
-            player_2_turn= False
+            print("board[{}][{}] is added with 2".format(line,p2))
+            player_2_turn = False
         if (win(board,2)):
             print("Player 2 Won the game")
             continue_game = False
